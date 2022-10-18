c80bb8b2e1af946da5bba18d62e54d36799eba7a
Jovin Thariyath <jovin.war@gmail.com>
2022-10-18 23:56:40 +0100

    Create binary-search.js

---
+++ binary-search.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let l=0;
+    let r=nums.length-1;
+    
+    while(l<=r){
+        
+        let mid = Math.floor((l+r)/2);
+        
+        if(nums[mid]> target){
+            r = mid-1
+        }else if(nums[mid]< target){
+            l = mid+1
+        }else if(nums[mid] === target){
+            
+            return mid
+        }
+    }
+    
+    return -1
+    
+};
